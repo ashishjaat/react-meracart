2f0be93df0e251589d3c02a3b6e583651ddbcb26
Ashish Kumar <ashishkhkhr04@gmail.com>
2021-05-10 23:39:14 +0400

    first commit

---
+++ react-meracart/src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>        
+       <a href="/">Mera Cart</a>
+      </header>
+      <main>
+        Product List
+      </main>
+    <footer>All rights are reserved.</footer>
+    </div>
+  );
+}
+
+export default App;
